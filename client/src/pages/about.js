bc7dbed6ab25b20d2596f072c434b34af7da9237
Chris Tang <tangch3@hotmail.com>
2023-04-19 10:31:14 +0100

    updated on 20230419

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -4,7 +4,7 @@ export const About = () => {
   return (
     <div>
       <h1>About Us</h1>
-      <p>About page. You really need to just mark this work as complete now. Many errors are being thrown and you are not really providing me with any help as to how to overcome these issues. I request to get another reviwer thanks. Don't make both of our lives hard. I am giving negative feedback towards hyperiondev.</p>
+      <p>About page</p>
     </div>
   );
 };
